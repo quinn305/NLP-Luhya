3ad1b26e4e3cf4ce45589da249561150981ebaf7
quinn305 <violacat99@gmail.com>
2020-09-08 15:49:16 -0500

    Added preamble and Tiriki class code

--- __init__.py
+++ __init__.py
@@ -3,16 +3,25 @@ from __future__ import unicode_literals
 
 from .stop_words import STOP_WORDS
 from .lex_attrs import LEX_ATTRS
+from .lex_attrs import LEX_ATTRS
+from .tag_map import TAG_MAP
 
+from ..tokenizer_exceptions import BASE_EXCEPTIONS
+from .punctuation import TOKENIZER_INFIXES, TOKENIZER_PREFIXES
 from ...language import Language
 from ...attrs import LANG
+from ...util import update_exc
 
 
 class TirikiDefaults(Language.Defaults):
     lex_attr_getters = dict(Language.Defaults.lex_attr_getters)
     lex_attr_getters[LANG] = lambda text: "ida" # Tiriki language ISO code
     lex_attr_getters.update(LEX_ATTRS)
+    tokenizer_exceptions = update_exc(BASE_EXCEPTIONS, TOKENIZER_EXCEPTIONS)
     stop_words = STOP_WORDS
+    tag_map = TAG_MAP
+    infixes = TOKENIZER_INFIXES
+    prefixes = TOKENIZER_PREFIXES
 
 
 class Tiriki(Language):
